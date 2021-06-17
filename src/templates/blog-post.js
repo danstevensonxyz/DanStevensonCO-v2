7c04e169ed6e7c70918e92b9e6125972a25a6b7f
DanStevensonCO <danstevensonco@gmail.com>
2021-06-17 15:53:24 +0100

    update date format

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -81,7 +81,7 @@ export const pageQuery = graphql`
       html
       frontmatter {
         title
-        date(formatString: "MMMM DD, YYYY")
+        date(formatString: "DD MMMM YYYY")
         description
       }
     }
