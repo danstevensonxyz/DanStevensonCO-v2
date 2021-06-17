bc152358af4b628e0b0a95c95f57b462737e035a
DanStevensonCO <danstevensonco@gmail.com>
2021-06-17 10:22:22 +0100

    centralise header text and remove social icons on small screens

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,20 +10,21 @@ const Layout = ({ children }) => {
     <div className="global-wrapper">
       <header class="header">
         <div class="icons">
+          
           <a href="#">
-            <GiHamburgerMenu size="1.5em" />
+            <GiHamburgerMenu id="header-hamburger-icon" size="1.5em"/>
           </a>
             
           <a href="https://github.com/DanStevensonCO/" target="_blank">
-            <AiOutlineGithub size="1.5em" />
+            <AiOutlineGithub id="header-github-icon" size="1.5em" />
           </a>
 
           <a href="https://twitter.com/DanStevensonCO/" target="_blank">
-            <FiTwitter size="1.5em" />
+            <FiTwitter id="header-twitter-icon" size="1.5em" />
           </a>
 
           <a href="https://linkedin.com/in/DanStevensonCO/" target="_blank">
-            <AiOutlineLinkedin size="1.5em" />
+            <AiOutlineLinkedin id="header-linkedin-icon" size="1.5em" />
           </a>
         </div>
         <h1>Dan Stevenson</h1>
