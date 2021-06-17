01e16a63ff81fbb20eb7bfa0d062f3d8a5e291bb
DanStevensonCO <danstevensonco@gmail.com>
2021-06-17 10:02:30 +0100

    add initial version of mobile and desktop header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,34 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
+import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
+import { FiTwitter } from 'react-icons/fi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+
 const Layout = ({ children }) => {
   return (
     <div className="global-wrapper">
-      <header>
-        <div className="menu">Menu here</div>
-        <nav>Nav links here</nav>
+      <header class="header">
+        <div class="icons">
+          <a href="#">
+            <GiHamburgerMenu size="1.5em" />
+          </a>
+            
+          <a href="https://github.com/DanStevensonCO/" target="_blank">
+            <AiOutlineGithub size="1.5em" />
+          </a>
+
+          <a href="https://twitter.com/DanStevensonCO/" target="_blank">
+            <FiTwitter size="1.5em" />
+          </a>
+
+          <a href="https://linkedin.com/in/DanStevensonCO/" target="_blank">
+            <AiOutlineLinkedin size="1.5em" />
+          </a>
+        </div>
+        <h1>Dan Stevenson</h1>
       </header>
-      {/* <main>{children}</main>
-      <footer></footer> */}
+      <main class="content">{children}</main>
     </div>
   )
 }
