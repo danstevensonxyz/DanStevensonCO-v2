c4644c50c7d69f3ca214c1e27932d3236fad5219
DanStevensonCO <danstevensonco@gmail.com>
2021-06-18 16:12:20 +0100

    add sliding nav bar, push main content to the right

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,32 @@ import { AiOutlineGithub, AiOutlineLinkedin } from 'react-icons/ai';
 import { FiTwitter } from 'react-icons/fi';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
+const toggleNav = () => {
+  const nav = document.getElementsByTagName("nav")[0]
+
+  console.log(nav)
+
+  if (nav.id === "nav--closed") {
+    nav.id = "nav--open"
+  } else {
+    nav.id = "nav--closed"
+  }
+}
+
 const Layout = ({ children }) => {
   return (
     <div className="global-wrapper">
+      <nav id="nav--closed">
+        <ul>
+          <li>Item 1</li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </ul>
+      </nav>
       <header class="header">
         <div class="icons">
           
-          <a href="#">
+          <a href="#" onClick={toggleNav}>
             <GiHamburgerMenu id="header-hamburger-icon" size="1.5em"/>
           </a>
             
